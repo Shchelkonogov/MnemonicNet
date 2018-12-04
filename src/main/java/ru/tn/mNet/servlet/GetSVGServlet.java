3901d7f83441d7d9f2b342e3c0d994ff23fd3d89
Maks <shchelkonogov@mail.ru>
2018-12-04 14:26:57 +0300

    Теперь больше не используется старый класс

--- src/main/java/ru/tn/mNet/servlet/GetSVGServlet.java
+++ src/main/java/ru/tn/mNet/servlet/GetSVGServlet.java
@@ -1,9 +1,15 @@
 package ru.tn.mNet.servlet;
 
 import ru.tn.mNet.bean.LoadNetData;
+import ru.tn.mNet.bean.LoadObjectParamData;
 import ru.tn.mNet.bean.LoadSvgContent;
 import ru.tn.mNet.model.*;
+import ru.tn.mNet.model.svgCreate.Attr;
+import ru.tn.mNet.model.svgCreate.AttrInter;
+import ru.tn.mNet.model.svgCreate.Tag;
+import ru.tn.mNet.model.svgCreate.TagInter;
 
+import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,7 +21,10 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Сервлет формирующий svg сети
@@ -25,20 +34,179 @@ public class GetSVGServlet extends HttpServlet {
 
     private HashMap<String, SVG> svgItemsMap = new HashMap<>();
 
-    @Inject
+    @EJB
     private LoadSvgContent bean;
 
-    @Inject
+    @EJB
     private LoadNetData netDataBean;
+
+    @EJB
+    private LoadObjectParamData objectParamDataBean;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Load mnemonic scheme net for: " + req.getParameter("objectId"));
 
-        int windowWidthMax = 1280;
-        double graphWidthMax = 0;
-        int objectsCount = 0;
         SVG svg;
+        Tag svgElement;
+        List<Tag> graphTags = new ArrayList<>();
+        int graphObjectsCount = 0;
+        final int netLength = 300;
+
+        List<NetModel> netData = netDataBean.loadData(req.getParameter("objectId"));
+        System.out.println("GraphData: " + netData);
+
+        double graphWidthMax = netData.stream().mapToDouble(NetModel::getLength).sum();
+
+        double xCurrentPosition;
+        double xNetLength = 0;
+        ObjectParamData paramData;
+        for(NetModel netItem: netData) {
+            System.out.println("Draw " + netItem.getSvgName());
+            if (!netItem.getSvgName().equals("Труба")) {
+                xCurrentPosition = graphObjectsCount * netLength;
+
+                paramData = objectParamDataBean.load(netItem.getObjectId());
+
+                //Добавляем объект на граф
+                svg = getSVG(netItem.getSvgName());
+                svgElement = new Tag(TagInter.GROUP, false);
+                svgElement.addAttr(new Attr(AttrInter.TRANSFORM, "translate(" + (xCurrentPosition - (svg.getWidth() / 2))
+                        + ", " + (640 - (svg.getHeight() / 2)) + ")"));
+//                svgElement.addStringTags(svg.getValue());
+
+                String test = insertDataToSVG(svg.getValue(), "Name", netItem.getName());
+                test = insertDataToSVG(test, "T1", paramData.getT1());
+                test = insertDataToSVG(test, "T2", paramData.getT2());
+
+//                svgElement.addStringTags(insertDataToSVG(svg.getValue(), "Name", netItem.getName()));
+                svgElement.addStringTags(test);
+                graphTags.add(svgElement);
+
+
+
+                //Добавляем вертикальные пунктирные линии для размера
+                svgElement = new Tag(TagInter.LINE, true);
+                svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.Y2, "730"),
+                        new Attr(AttrInter.X2, String.valueOf(xCurrentPosition)),
+                        new Attr(AttrInter.Y1, String.valueOf(800
+                                + ((graphObjectsCount - 1) * ((netData.indexOf(netItem) == 0 ? 0 : getSVG("arrow_right").getHeight()) + 5))
+                                + (graphObjectsCount == 0 ? 5 : 0))),
+                        new Attr(AttrInter.X1, String.valueOf(xCurrentPosition)),
+                        new Attr("stroke-linecap", "square"),
+                        new Attr("stroke-dasharray", "5,5"),
+                        new Attr(AttrInter.STROKE, "#000000"),
+                        new Attr(AttrInter.FILL, "none")));
+                graphTags.add(svgElement);
+
+                if (netData.indexOf(netItem) != 0) {
+
+                    svg = getSVG("Переход");
+                    svgElement = new Tag(TagInter.GROUP, false);
+                    svgElement.addAttr(new Attr(AttrInter.TRANSFORM, "translate(" + (xCurrentPosition - (netLength / 2) - (svg.getWidth() / 2))
+                            + ", " + (640 - (svg.getHeight() / 2)) + ")"));
+                    svgElement.addStringTags(svg.getValue());
+                    graphTags.add(svgElement);
+
+
+                    //добавляем левые стрелочки для размера
+                    svg = getSVG("arrow_left");
+
+                    double yPosition = 800 + (graphObjectsCount * 5) + ((graphObjectsCount - 1) * svg.getHeight());
+
+                    svgElement = new Tag(TagInter.GROUP, false);
+                    svgElement.addAttr(new Attr(AttrInter.TRANSFORM,
+                            "translate(-1, " + yPosition + ")"));
+                    svgElement.addStringTags(svg.getValue());
+                    graphTags.add(svgElement);
+
+                    //Добавляем правые стрелки для размера
+                    svg = getSVG("arrow_right");
+                    svgElement = new Tag(TagInter.GROUP, false);
+                    svgElement.addAttr(new Attr(AttrInter.TRANSFORM,
+                            "translate(" + (xCurrentPosition - svg.getWidth()) + ", " + yPosition + ")"));
+                    svgElement.addStringTags(svg.getValue());
+                    graphTags.add(svgElement);
+
+                    double yLineTextPosition = yPosition + (svg.getHeight() / 2);
+
+                    //Добавляем горизонтальную линию размера
+                    svgElement = new Tag(TagInter.LINE, true);
+                    svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.Y2, String.valueOf(yLineTextPosition)),
+                            new Attr(AttrInter.X2, String.valueOf(xCurrentPosition - svg.getWidth())),
+                            new Attr(AttrInter.Y1, String.valueOf(yLineTextPosition)),
+                            new Attr(AttrInter.X1, String.valueOf(svg.getWidth() + 1)),
+                            new Attr(AttrInter.STROKE, "#000000"),
+                            new Attr(AttrInter.FILL, "none")));
+                    graphTags.add(svgElement);
+
+                    double xTextPosition = ((svg.getWidth() + 1) + xCurrentPosition - svg.getWidth()) / 2;
+
+                    //Добавляем текст с расстоянием к размерам
+                    svgElement = new Tag(TagInter.TEXT, false);
+                    svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.XMLNS, "http://www.w3.org/2000/svg"),
+                            new Attr("xml:space", "preserve"),
+//                            new Attr("text-anchor", "middle"),
+                            new Attr("text-anchor", "start"),
+                            new Attr("font-family", "serif"),
+                            new Attr("font-size", "16"),
+                            new Attr("y", String.valueOf(yLineTextPosition - 3)),
+//                            new Attr("x", String.valueOf(xTextPosition)),
+                            new Attr("x", String.valueOf(svg.getWidth() + 1)),
+                            new Attr(AttrInter.STROKE_WIDTH, "0"),
+                            new Attr(AttrInter.FILL, "#000000")));
+                    svgElement.setValue(String.valueOf("L: " + new BigDecimal(String.valueOf(xNetLength))
+                            .setScale(2, RoundingMode.HALF_EVEN)) + "м");
+                    graphTags.add(svgElement);
+
+                    //Добавляем текст со временем к размерам
+                    String timeDirect = "tп: " + LocalTime.ofSecondOfDay(new BigDecimal(String.valueOf(xNetLength / 1.6))
+                            .setScale(0, RoundingMode.HALF_EVEN)
+                            .longValueExact())
+                            .format(DateTimeFormatter.ofPattern("HHч mmм ssс"));
+                    String timeReverse = " tо: " + LocalTime.ofSecondOfDay(
+                            new BigDecimal(String.valueOf((2 * graphWidthMax - xNetLength) / 1.6))
+                            .setScale(0, RoundingMode.HALF_EVEN)
+                            .longValueExact())
+                            .format(DateTimeFormatter.ofPattern("HHч mmм ssс"));
+
+                    svgElement = new Tag(TagInter.TEXT, false);
+                    svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.XMLNS, "http://www.w3.org/2000/svg"),
+                            new Attr("xml:space", "preserve"),
+                            new Attr("text-anchor", "end"),
+                            new Attr("font-family", "serif"),
+                            new Attr("font-size", "16"),
+//                            new Attr("y", String.valueOf(yLineTextPosition + 13)),
+                            new Attr("y", String.valueOf(yLineTextPosition - 3)),
+//                            new Attr("x", String.valueOf(xTextPosition)),
+                            new Attr("x", String.valueOf(xCurrentPosition - svg.getWidth())),
+                            new Attr(AttrInter.STROKE_WIDTH, "0"),
+                            new Attr(AttrInter.FILL, "#000000")));
+//                    svgElement.setValue(timeDirect + timeReverse);
+                    svgElement.setValue(timeDirect);
+                    graphTags.add(svgElement);
+
+                    svgElement = new Tag(TagInter.TEXT, false);
+                    svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.XMLNS, "http://www.w3.org/2000/svg"),
+                            new Attr("xml:space", "preserve"),
+                            new Attr("text-anchor", "end"),
+                            new Attr("font-family", "serif"),
+                            new Attr("font-size", "16"),
+                            new Attr("y", String.valueOf(yLineTextPosition + 13)),
+                            new Attr("x", String.valueOf(xCurrentPosition - svg.getWidth())),
+                            new Attr(AttrInter.STROKE_WIDTH, "0"),
+                            new Attr(AttrInter.FILL, "#000000")));
+                    svgElement.setValue(timeReverse);
+                    graphTags.add(svgElement);
+                }
+
+                graphObjectsCount++;
+            } else {
+                xNetLength += netItem.getLength();
+            }
+        }
+
+        int windowWidthMax = (graphObjectsCount - 1) * netLength;
 
         Tag rootTag = new Tag(TagInter.SVG, false);
         rootTag.addAttrs(Arrays.asList(new Attr(AttrInter.WIDTH, String.valueOf(windowWidthMax)),
@@ -48,7 +216,7 @@ public class GetSVGServlet extends HttpServlet {
                 new Attr("xmlns:xlink", "http://www.w3.org/1999/xlink")));
 
         //Линия на которой размещаются все объекты графа
-        Tag svgElement = new Tag(TagInter.LINE, true);
+        svgElement = new Tag(TagInter.LINE, true);
         svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.Y2, "640"),
                 new Attr(AttrInter.X2, String.valueOf(windowWidthMax)),
                 new Attr(AttrInter.Y1, "640"),
@@ -58,117 +226,7 @@ public class GetSVGServlet extends HttpServlet {
                 new Attr(AttrInter.FILL, "none")));
         rootTag.addTag(svgElement);
 
-        List<NetModel> netData = netDataBean.loadData(req.getParameter("objectId"));
-        System.out.println("Graph data: " + netData);
-
-        for (NetModel item: netData) {
-            graphWidthMax += item.getLength();
-        }
-
-        double xCurrentPosition = 0;
-        double proportion;
-        for(NetModel netItem: netData) {
-            System.out.println("Draw " + netItem.getSvgName());
-            if (!netItem.getSvgName().equals("Труба")) {
-                proportion = (xCurrentPosition * windowWidthMax) / graphWidthMax;
-
-                //Добавляем объект на граф
-                svg = getSVG(netItem.getSvgName());
-                svgElement = new Tag(TagInter.GROUP, false);
-                svgElement.addAttr(new Attr(AttrInter.TRANSFORM, "translate(" + (proportion - (svg.getWidth() / 2))
-                        + ", " + (640 - (svg.getHeight() / 2)) + ")"));
-                svgElement.addStringTags(svg.getValue());
-                rootTag.addTag(svgElement);
-
-                if (netData.indexOf(netItem) != 0) {
-                    objectsCount++;
-
-                    //Добавляем правые стрелки для размера
-                    svg = getSVG("arrow_right");
-                    svgElement = new Tag(TagInter.GROUP, false);
-                    svgElement.addAttr(new Attr(AttrInter.TRANSFORM, "translate(" + (proportion - svg.getWidth())
-                            + ", " + (840 + (5 * objectsCount) + (svg.getHeight() * (objectsCount - 1))) + ")"));
-                    svgElement.addStringTags(svg.getValue());
-                    rootTag.addTag(svgElement);
-
-                    //Добавляем горизонтальную линию размера
-                    double yLinePosition = 840 + (5 * objectsCount) + ((objectsCount - 1) * svg.getHeight()) + (svg.getHeight() / 2);
-                    svgElement = new Tag(TagInter.LINE, true);
-                    svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.Y2, String.valueOf(yLinePosition)),
-                            new Attr(AttrInter.X2, String.valueOf(proportion - svg.getWidth())),
-                            new Attr(AttrInter.Y1, String.valueOf(yLinePosition)),
-                            new Attr(AttrInter.X1, String.valueOf(svg.getWidth() + 1)),
-                            new Attr(AttrInter.STROKE, "#000000"),
-                            new Attr(AttrInter.FILL, "none")));
-                    rootTag.addTag(svgElement);
-
-                    double yTextPosition = 840 + (5 * objectsCount) + ((objectsCount - 1) * svg.getHeight()) + (svg.getHeight() / 2);
-                    double xTextPosition = (proportion - svg.getWidth() + (svg.getWidth() + 1)) / 2;
-
-                    //Добавляем текст с расстоянием к размерам
-                    svgElement = new Tag(TagInter.TEXT, false);
-                    svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.XMLNS, "http://www.w3.org/2000/svg"),
-                            new Attr("xml:space", "preserve"),
-                            new Attr("text-anchor", "middle"),
-                            new Attr("font-family", "serif"),
-                            new Attr("font-size", "16"),
-                            new Attr("y", String.valueOf(yTextPosition - 3)),
-                            new Attr("x", String.valueOf(xTextPosition)),
-                            new Attr(AttrInter.STROKE_WIDTH, "0"),
-                            new Attr(AttrInter.FILL, "#000000")));
-                    svgElement.setValue(String.valueOf(new BigDecimal(String.valueOf(xCurrentPosition)).setScale(2, RoundingMode.HALF_EVEN)) + "м");
-                    rootTag.addTag(svgElement);
-
-                    //Добавляем текст со временем к размерам
-                    String timeDirect = "tп=" + LocalTime.ofSecondOfDay(new BigDecimal(String.valueOf(xCurrentPosition / 1.6))
-                            .setScale(0, RoundingMode.HALF_EVEN)
-                            .longValueExact())
-                            .format(DateTimeFormatter.ofPattern("HHч mmм ssс"));
-                    String timeReverse = " tо=" + LocalTime.ofSecondOfDay(new BigDecimal(String.valueOf((2 * graphWidthMax - xCurrentPosition) / 1.6))
-                            .setScale(0, RoundingMode.HALF_EVEN)
-                            .longValueExact())
-                            .format(DateTimeFormatter.ofPattern("HHч mmм ssс"));
-
-                    svgElement = new Tag(TagInter.TEXT, false);
-                    svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.XMLNS, "http://www.w3.org/2000/svg"),
-                            new Attr("xml:space", "preserve"),
-                            new Attr("text-anchor", "middle"),
-                            new Attr("font-family", "serif"),
-                            new Attr("font-size", "16"),
-                            new Attr("y", String.valueOf(yTextPosition + 13)),
-                            new Attr("x", String.valueOf(xTextPosition)),
-                            new Attr(AttrInter.STROKE_WIDTH, "0"),
-                            new Attr(AttrInter.FILL, "#000000")));
-                    svgElement.setValue(timeDirect + timeReverse);
-                    rootTag.addTag(svgElement);
-                }
-
-                //Добавляем вертикальные пунктирные линии для размера
-                svgElement = new Tag(TagInter.LINE, true);
-                svgElement.addAttrs(Arrays.asList(new Attr(AttrInter.Y2, "730"),
-                        new Attr(AttrInter.X2, String.valueOf(proportion)),
-                        new Attr(AttrInter.Y1, String.valueOf(840
-                                + ((objectsCount - 1) * ((netData.indexOf(netItem) == 0 ? 0 : getSVG("arrow_right").getHeight()) + 5))
-                                + (objectsCount == 0 ? 5 : 0))),
-                        new Attr(AttrInter.X1, String.valueOf(proportion)),
-                        new Attr("stroke-linecap", "square"),
-                        new Attr("stroke-dasharray", "5,5"),
-                        new Attr(AttrInter.STROKE, "#000000"),
-                        new Attr(AttrInter.FILL, "none")));
-                rootTag.addTag(svgElement);
-            } else {
-                xCurrentPosition += netItem.getLength();
-            }
-        }
-
-        //Добавляем левые стрелочки для обозначения расстояния
-        svg = getSVG("arrow_left");
-        for (int i = 0; i < objectsCount; i++) {
-            svgElement = new Tag(TagInter.GROUP, false);
-            svgElement.addAttr(new Attr(AttrInter.TRANSFORM, "translate(-1, " + (840 + 5 + (i * 5) + (i * svg.getHeight())) + ")"));
-            svgElement.addStringTags(svg.getValue());
-            rootTag.addTag(svgElement);
-        }
+        graphTags.forEach(rootTag::addTag);
 
         String contentS = "<?xml version=\"1.0\"?>" + rootTag;
 //        System.out.println(contentS);
@@ -186,5 +244,10 @@ public class GetSVGServlet extends HttpServlet {
             svgItemsMap.put(name, new SVG(bean.getSvg(name)));
         }
         return svgItemsMap.get(name);
+//        return new SVG(bean.getSvg(name));
+    }
+
+    private String insertDataToSVG(String svgPart, String id, String value) {
+        return svgPart.replaceAll("(<text .*id=\"" + id + "\".*>)(.*)(</text>)", "$1" + value + "$3");
     }
 }
